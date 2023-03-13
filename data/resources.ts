484571e333810fbe7623830c568fe32301a3bed4
imtiaz0307 <imtiaz0307@gmail.com>
2023-03-13 13:42:59 +0500

    Populated the data and added the dynamic link to jobitem.

--- data/resources.ts
+++ data/resources.ts
@@ -18,93 +18,101 @@ export const resources = [
         link: "https://www.youtube.com/watch?v=0fYi8SGA20k"
     },
     {
-        banner: "https://i.ytimg.com/vi/RGKi6LSPDLU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFyMIk5bi9yyFKaxXIvjyZCaHKGA",
-        title: "Learn React in one video.",
-        description: "Learn React in one video by a great youtuber, programmer, and a live saver for millions of programmers out there known as CodeWithHarry.",
-        link: "https://www.youtube.com/watch?v=RGKi6LSPDLU&t=12s"
+        banner:
+            "https://i.ytimg.com/vi/YaioUnMw0mo/hq720.jpg?sqp=CIzN3PoF-oYCogNfzOYRgNoF&rs=AOn4CLDVuFJGVs-3AujIq3aYv_8zgPZoVQ",
+        title: "Python Programming for Beginners",
+        description:
+            "This video tutorial by Programming with Mosh is a great introduction to Python programming for beginners. It covers the basics of Python syntax and programming concepts such as variables, loops, and functions.",
+        link: "https://www.youtube.com/watch?v=YaioUnMw0mo",
     },
     {
-        banner: "https://i.ytimg.com/vi/T63nY70eZF0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5Jxy7fh5kO6cP9baZAmqPHxVxaw",
-        title: "NextJs 13 crash course.",
-        description: "DevelopedByEd is one of the best web development youtube channel to learn various web related technologies and actually master them.",
-        link: "https://www.youtube.com/watch?v=T63nY70eZF0&t=2411s"
+        banner:
+            "https://i.ytimg.com/vi/YXPyB4XeYLA/hq720.jpg?sqp=CIzN3PoF-oYCogNfzOYRgNoF&rs=AOn4CLCt7NHuELTJdV7ePGOJ-tpcH8LYQw",
+        title: "Learn HTML and CSS",
+        description:
+            "This video tutorial by FreeCodeCamp.org is a comprehensive introduction to HTML and CSS for beginners. It covers the basics of HTML tags and CSS styling, as well as more advanced concepts such as responsive design and CSS frameworks.",
+        link: "https://www.youtube.com/watch?v=YXPyB4XeYLA",
     },
     {
-        banner: "https://i.ytimg.com/vi/0fYi8SGA20k/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCd17X5LmMqtANl7WsQU-OQrhKm4w",
-        title: "Learn Three.js In One Video.",
-        description: "Learn Three.js by building and deploying your portfolio website by Javascript Master, one of the best youtube channel to actually master JavaScript.",
-        link: "https://www.youtube.com/watch?v=0fYi8SGA20k"
+        banner:
+            "https://i.ytimg.com/vi/Fy9SdZLBTOo/hq720.jpg?sqp=CIzN3PoF-oYCogNfzOYRgNoF&rs=AOn4CLB00HvziS5S1oiYK2oGWCGM4KPvHw",
+        title: "JavaScript Crash Course",
+        description:
+            "This video tutorial by Traversy Media is a fast-paced introduction to JavaScript for beginners. It covers the basics of JavaScript syntax, data types, and control structures, as well as more advanced topics such as functions and object-oriented programming.",
+        link: "https://www.youtube.com/watch?v=Fy9SdZLBTOo",
     },
     {
-        banner: "https://i.ytimg.com/vi/RGKi6LSPDLU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFyMIk5bi9yyFKaxXIvjyZCaHKGA",
-        title: "Learn React in one video.",
-        description: "Learn React in one video by a great youtuber, programmer, and a live saver for millions of programmers out there known as CodeWithHarry.",
-        link: "https://www.youtube.com/watch?v=RGKi6LSPDLU&t=12s"
+        banner:
+            "https://i.ytimg.com/vi/w8yWXqWQYmU/hq720.jpg?sqp=CIzN3PoF-oYCogNfzOYRgNoF&rs=AOn4CLDhNweS7V8JN-kU-Q7VUOvL9XixDw",
+        title: "Data Structures and Algorithms in Java",
+        description:
+            "This video tutorial by Kevin Naughton Jr. covers a range of data structures and algorithms using Java. It includes topics such as arrays, linked lists, sorting and searching algorithms, and dynamic programming.",
+        link: "https://www.youtube.com/watch?v=w8yWXqWQYmU",
     },
     {
-        banner: "https://i.ytimg.com/vi/T63nY70eZF0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5Jxy7fh5kO6cP9baZAmqPHxVxaw",
-        title: "NextJs 13 crash course.",
-        description: "DevelopedByEd is one of the best web development youtube channel to learn various web related technologies and actually master them.",
-        link: "https://www.youtube.com/watch?v=T63nY70eZF0&t=2411s"
+        banner: "https://i.ytimg.com/vi/e8lYLYlrGLg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBhefPjKwZzJ3iKjTccTThIeX9x-Q",
+        title: "Node.js Crash Course",
+        description: "A fast-paced Node.js tutorial that teaches you how to build scalable, reliable, and performant applications with Node.js, Express, and MongoDB. By Traversy Media.",
+        link: "https://www.youtube.com/watch?v=e8lYLYlrGLg"
     },
     {
-        banner: "https://i.ytimg.com/vi/0fYi8SGA20k/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCd17X5LmMqtANl7WsQU-OQrhKm4w",
-        title: "Learn Three.js In One Video.",
-        description: "Learn Three.js by building and deploying your portfolio website by Javascript Master, one of the best youtube channel to actually master JavaScript.",
-        link: "https://www.youtube.com/watch?v=0fYi8SGA20k"
+        banner: "https://i.ytimg.com/vi/nmHfr1jG9fg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA8Y5Q5ieHflZrUaL6U_p8yyX9Kzw",
+        title: "Vue.js 3 Crash Course",
+        description: "A beginner-friendly introduction to Vue.js 3 that covers the fundamentals of the framework, including components, directives, and reactive data. By FreeCodeCamp.",
+        link: "https://www.youtube.com/watch?v=nmHfr1jG9fg"
     },
     {
-        banner: "https://i.ytimg.com/vi/RGKi6LSPDLU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFyMIk5bi9yyFKaxXIvjyZCaHKGA",
-        title: "Learn React in one video.",
-        description: "Learn React in one video by a great youtuber, programmer, and a live saver for millions of programmers out there known as CodeWithHarry.",
-        link: "https://www.youtube.com/watch?v=RGKi6LSPDLU&t=12s"
+        banner: "https://i.ytimg.com/vi/wv1GUTWtSn0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAX6Z2jG_mJNC6ofxJ6JmDT0hkJTw",
+        title: "Docker Tutorial for Beginners",
+        description: "A comprehensive tutorial that teaches you how to use Docker to containerize your applications, and deploy them to the cloud. By Edureka.",
+        link: "https://www.youtube.com/watch?v=wv1GUTWtSn0"
     },
     {
-        banner: "https://i.ytimg.com/vi/T63nY70eZF0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5Jxy7fh5kO6cP9baZAmqPHxVxaw",
-        title: "NextJs 13 crash course.",
-        description: "DevelopedByEd is one of the best web development youtube channel to learn various web related technologies and actually master them.",
-        link: "https://www.youtube.com/watch?v=T63nY70eZF0&t=2411s"
+        banner: "https://i.ytimg.com/vi/-G7bJVAIiEI/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA4_yFomOW4Q3qjVTNjkwgTTdNldg",
+        title: "AWS Lambda Tutorial for Beginners",
+        description: "A step-by-step tutorial that teaches you how to use AWS Lambda to build serverless applications in the cloud. By Simplilearn.",
+        link: "https://www.youtube.com/watch?v=-G7bJVAIiEI"
     },
     {
-        banner: "https://i.ytimg.com/vi/0fYi8SGA20k/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCd17X5LmMqtANl7WsQU-OQrhKm4w",
-        title: "Learn Three.js In One Video.",
-        description: "Learn Three.js by building and deploying your portfolio website by Javascript Master, one of the best youtube channel to actually master JavaScript.",
-        link: "https://www.youtube.com/watch?v=0fYi8SGA20k"
+        banner: "https://i.ytimg.com/vi/gzJ6uo_Db6o/hq720.jpg",
+        title: "Bootstrap 5 Crash Course For Beginners",
+        description: "In this Bootstrap 5 tutorial, you'll learn everything you need to know to get started building beautiful responsive websites with the latest version of Bootstrap.",
+        link: "https://www.youtube.com/watch?v=gzJ6uo_Db6o"
     },
     {
-        banner: "https://i.ytimg.com/vi/RGKi6LSPDLU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFyMIk5bi9yyFKaxXIvjyZCaHKGA",
-        title: "Learn React in one video.",
-        description: "Learn React in one video by a great youtuber, programmer, and a live saver for millions of programmers out there known as CodeWithHarry.",
-        link: "https://www.youtube.com/watch?v=RGKi6LSPDLU&t=12s"
+        banner: "https://i.ytimg.com/vi/4u0jgEZWf_U/hq720.jpg",
+        title: "The Complete 2021 Flutter Development Bootcamp with Dart",
+        description: "This course teaches you how to build beautiful, fast, and native-quality iOS and Android apps with Flutter.",
+        link: "https://www.youtube.com/watch?v=4u0jgEZWf_U"
     },
     {
-        banner: "https://i.ytimg.com/vi/T63nY70eZF0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5Jxy7fh5kO6cP9baZAmqPHxVxaw",
-        title: "NextJs 13 crash course.",
-        description: "DevelopedByEd is one of the best web development youtube channel to learn various web related technologies and actually master them.",
-        link: "https://www.youtube.com/watch?v=T63nY70eZF0&t=2411s"
+        banner: "https://i.ytimg.com/vi/wTcU2-C3fCg/hq720.jpg",
+        title: "Vue.js 3 Essential Training",
+        description: "In this course, learn how to leverage the latest features of Vue 3 to build highly scalable, modular, and responsive user interfaces for your web applications.",
+        link: "https://www.youtube.com/watch?v=wTcU2-C3fCg"
     },
     {
-        banner: "https://i.ytimg.com/vi/0fYi8SGA20k/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCd17X5LmMqtANl7WsQU-OQrhKm4w",
-        title: "Learn Three.js In One Video.",
-        description: "Learn Three.js by building and deploying your portfolio website by Javascript Master, one of the best youtube channel to actually master JavaScript.",
-        link: "https://www.youtube.com/watch?v=0fYi8SGA20k"
+        banner: "https://i.ytimg.com/vi/OMJTA3qP1OM/hq720.jpg",
+        title: "Learn Node.js - Full Tutorial for Beginners",
+        description: "In this complete Node.js tutorial, you'll learn how to get started with Node.js, build your own web applications, and even create your own APIs.",
+        link: "https://www.youtube.com/watch?v=OMJTA3qP1OM"
     },
     {
-        banner: "https://i.ytimg.com/vi/RGKi6LSPDLU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFyMIk5bi9yyFKaxXIvjyZCaHKGA",
-        title: "Learn React in one video.",
-        description: "Learn React in one video by a great youtuber, programmer, and a live saver for millions of programmers out there known as CodeWithHarry.",
-        link: "https://www.youtube.com/watch?v=RGKi6LSPDLU&t=12s"
+        banner: "https://i.ytimg.com/vi/_JU6Qslenwc/hq720.jpg",
+        title: "Git & GitHub Crash Course For Beginners",
+        description: "In this Git and GitHub crash course, you'll learn how to use Git and GitHub to manage your code, collaborate with others, and even deploy your own websites.",
+        link: "https://www.youtube.com/watch?v=_JU6Qslenwc"
     },
     {
-        banner: "https://i.ytimg.com/vi/T63nY70eZF0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5Jxy7fh5kO6cP9baZAmqPHxVxaw",
-        title: "NextJs 13 crash course.",
-        description: "DevelopedByEd is one of the best web development youtube channel to learn various web related technologies and actually master them.",
-        link: "https://www.youtube.com/watch?v=T63nY70eZF0&t=2411s"
+        banner: "https://i.ytimg.com/vi/5kHyviqjhCk/hq720.jpg",
+        title: "Complete React Tutorial for Beginners (2021)",
+        description: "In this React tutorial for beginners, you'll learn everything you need to know to build beautiful and interactive web applications with React.",
+        link: "https://www.youtube.com/watch?v=5kHyviqjhCk"
     },
     {
-        banner: "https://i.ytimg.com/vi/0fYi8SGA20k/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCd17X5LmMqtANl7WsQU-OQrhKm4w",
-        title: "Learn Three.js In One Video.",
-        description: "Learn Three.js by building and deploying your portfolio website by Javascript Master, one of the best youtube channel to actually master JavaScript.",
-        link: "https://www.youtube.com/watch?v=0fYi8SGA20k"
+        banner: "https://i.ytimg.com/vi/YSkzOkxcxlI/hq720.jpg",
+        title: "React Native Crash Course 2021",
+        description: "In this React Native crash course, you'll learn how to build native-quality iOS and Android apps using only JavaScript and React.",
+        link: "https://www.youtube.com/watch?v=YSkzOkxcxlI"
     },
 ]
