1b801de204e45ed126617d54c4f4345f7fb1e410
imtiaz0307 <imtiaz0307@gmail.com>
2023-03-16 00:43:30 +0500

    worked on the login functionality on both server and client.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,16 +1,42 @@
+"use client"
+
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [error, setError] = useState<string>("")
+
+    const loginHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const res = await fetch('/api/auth/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        })
+        const data = await res.json()
+        if (data.error) {
+            data.error.includes("credentials") && setError(data.error)
+            setTimeout(() => setError(""), 3000)
+        }
+        if (data.success) {
+            localStorage.setItem("auth-token", data.token)
+        }
+    }
     return (
         <main className='flex justify-center items-center py-12 px-6'>
             <section className='flex border-blue-200 border-2 rounded-[10px] overflow-hidden w-full max-w-[800px]'>
-                <form className='flex-[1.2] flex flex-col justify-center p-4 items-center'>
+                <form method='POST' onSubmit={loginHandler} className='flex-[1.2] flex flex-col justify-center p-4 items-center'>
                     <h2 className='text-blue-500 text-[2rem] font-[700] text-center mb-12'>Login to your account!</h2>
-                    <input type="email" id='email' placeholder='Enter your email address..' className='p-4 border-blue-200 rounded border-2 w-full max-w-[400px] mb-6' />
-                    <input type="password" placeholder='Enter your password' className='p-4 border-blue-200 rounded border-2 w-full max-w-[400px] mb-6' />
-                    <button type='submit' className='bg-blue-500 text-white py-4 rounded w-full max-w-[400px]'>Login</button>
+                    <input type="email" id='email' placeholder='Enter your email address..' className='p-4 border-blue-200 rounded border-2 w-full max-w-[400px] mb-6' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder='Enter your password' className='p-4 border-blue-200 rounded border-2 w-full max-w-[400px]' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    {error && <span className='text-sm text-red-500 text-center py-3'>{error}</span>}
+                    <button type='submit' className='bg-blue-500 text-white py-4 rounded w-full max-w-[400px] mt-6'>Login</button>
                     <p className='pt-4 text-sm'>Don't have an account? <Link href={"/auth/signup"} className="text-blue-600 underline">Signup.</Link></p>
                 </form>
                 <div className='flex-1 bg-gradient-to-r from-sky-500 to-indigo-500 rounded-[10px]'>
---
+++ pages/api/auth/login.ts
@@ -0,0 +1,38 @@
+import { User } from "@/models/User";
+import { dbConnection } from "@/utils/DbConnection";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    // database connection handler 
+    await dbConnection()
+
+    // if request method is not post
+    if (req.method !== "POST") return res.status(400).json({ error: "Only post requests are allowed." })
+
+    try {
+        // if user with the entered email doesn't exist
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) return res.status(401).json({ error: "Invalid credentials." })
+
+        // if the password is incorrent
+        const password = await compare(req.body.password, user.password)
+        if (!password) return res.status(401).json({ error: "Invalid credentials." })
+
+        // auth_token
+        const payload = {
+            user: {
+                id: user._id
+            }
+        }
+
+        const token = sign(payload, process.env.JWT_SECRET!)
+
+        res.status(200).json({ token, success: "User logged in successfully." })
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error." })
+    }
+}
+
+export default handler;
